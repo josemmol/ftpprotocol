66911a9afb9a87319c9f2409af3837591166212a
Jose Maria Madroñal <jmmadronal@gmail.com>
2024-01-19 23:54:46 +0100

    commit por

--- clientFTPDATA.py
+++ clientFTPDATA.py
@@ -1,9 +1,39 @@
 import socket
+import psutil
+
+def get_address_ip():
+    try:
+        # Obté el nom del host
+        host_name = socket.gethostname()
+        interfaces_de_red = psutil.net_if_addrs()
+
+        # Obté l'adreça IP asociada al nom del host
+        address_ip = socket.gethostbyname(host_name)
+        # Busca la primera interfaz que esté conectada a la red local y obtiene su dirección IP
+        address_ip = None
+        for interface, direcciones in interfaces_de_red.items():
+            for direccion in direcciones:
+                if direccion.family == socket.AF_INET and not direccion.address.startswith("127."):
+                    address_ip = direccion.address
+                    break
+            if address_ip:
+                break
+
+        if address_ip:
+            print(f"La dirección IP de la interfaz de red conectada a la red local es: {address_ip}")
+        else:
+            print("No se encontró una interfaz de red conectada a la red local.")
+
+        return address_ip 
+
+    except Exception as e:
+        print(f"Error al obtenir l'addreça IP: {e}")
 
 def main():
+    address_ip = get_address_ip()
     # Configura el host y el puerto
-    host = '127.0.0.1'  # Puedes cambiarlo por la dirección IP de tu máquina
-    port = 40000
+    host = address_ip  # Puedes cambiarlo por la dirección IP de tu máquina
+    port = 40000 
 
     # Crea un objeto socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
